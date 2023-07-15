e065198532157eb635e5eac3da3ff8a5b4196830
Megakyper <128244277+Megakyper@users.noreply.github.com>
2023-07-16 00:12:43 +0530

    final

--- total/index.js
+++ total/index.js
@@ -74,5 +74,51 @@ for (var j = 0; j < navs.length; j++) {
             cel.classList.remove("show");
             cel.classList.remove("remove");
         }, 300);
+        cel.classList.add("show");
     };
+}
+
+let lig = document.getElementById('lig');
+let l;
+for (var h = 0; h < 95; h++) {
+    let g = lig.innerHTML;
+    lig.innerHTML = g + '<div class="ligs"></div>';
+}
+
+setInterval(getCursor, 100)
+
+function getCursor() {
+    var w = window.innerWidth;
+    let d = document.getElementsByClassName('ligs');
+    var t = null;
+    if (w >= 1024)
+        t = 20;
+
+    if (w < 1024 && w > 800)
+        t = 15;
+
+    if (w <= 800)
+        t = 10;
+
+    for (let j = 0; j < d.length; j++) {
+        d[j].style.right = (25 + (j * 50) / d.length) + '%';
+        if (j <= ((d.length) / 4)) {
+            d[j].style.animationDelay = ((((d.length / 4) - j) * t) / (d.length)) + 's';
+        }
+
+        if (j < ((d.length) / 2) && j > ((d.length) / 4)) {
+            d[j].style.animationDelay = (((j - (d.length / 4)) * ((3 * t) / 4)) / (d.length)) + 's';
+            d[j].style.background = 'linear-gradient(131.5deg, rgb(350,25,25)3%' + (90 - ((((((d.length) / 2) - 1 - j) * 1200) / (d.length)))) + '%, rgb(145, 145, 145) 0% 97% )'
+        }
+
+        if (j >= ((d.length) / 2) && j < ((3 * d.length) / 4))
+            d[j].style.animationDelay = (((((3 * d.length) / 4) - j) * ((3 * t) / 4)) / (d.length)) + 's';
+
+        if (j > ((3 * d.length) / 4))
+            d[j].style.animationDelay = (((j - ((3 * d.length) / 4)) * t) / (d.length)) + 's';
+
+        if (j >= ((d.length) / 2))
+            d[j].style.background = 'rgb(350,25,25) '
+
+    }
 }
